3f7a0e581688a70224899b27987f74a3feef76f4
FM <mirshekarweb@gmail.com>
2020-05-03 21:03:54 +0430

    working dashboard user

---
+++ Ahora.WebApp/Areas/User/Controllers/ProfileController.cs
@@ -0,0 +1,34 @@
+﻿using FM.Portal.Core.Common;
+using FM.Portal.Core.Model;
+using FM.Portal.Core.Service;
+using FM.Portal.FrameWork.MVC.Controller;
+using System.Web.Mvc;
+
+namespace Ahora.WebApp.Areas.User.Controllers
+{
+    public class ProfileController : BaseController<IUserService>
+    {
+        public ProfileController(IUserService service) : base(service)
+        {
+        }
+
+        // GET: User/Profile
+        public ActionResult Index()
+        {
+            var userResult = _service.Get(SQLHelper.CheckGuidNull(User.Identity.Name));
+            if (!userResult.Success)
+                return View("Error");
+            var user = userResult.Data;
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult Edit(FM.Portal.Core.Model.User model)
+        {
+            var userResult = _service.Update(model);
+            if (!userResult.Success)
+                return View("Error");
+            var user = userResult.Data;
+            return RedirectToAction("Index");
+        }
+    }
+}
