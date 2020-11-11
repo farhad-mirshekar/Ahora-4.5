a9c9554c81b4e8d1a55226659b66038d26682cfa
FM <mirshekarweb@gmail.com>
2020-11-11 21:54:02 +0330

    .

---
+++ Ahora.WebApp/Areas/Admin/Controllers/Log/ActivityLogController.cs
@@ -0,0 +1,15 @@
+﻿using FM.Portal.FrameWork.Attributes;
+using System.Web.Mvc;
+
+namespace Ahora.WebApp.Areas.Admin.Controllers
+{
+    [UserAuthorize(Roles = "Admin")]
+    public class ActivityLogController : Controller
+    {
+        // GET: Admin/ActivityLog
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
